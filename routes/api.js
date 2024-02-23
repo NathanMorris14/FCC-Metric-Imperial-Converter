eea6bb57aebd437dad32cd3d20c8bed5cd5c7f09
Replit user <>
2024-02-23 12:08:57 -0500

    Added code project
    this commit contains all the working code

--- routes/api.js
+++ routes/api.js
@@ -6,5 +6,36 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
+  
+  app.route('/api/convert').get(function(req, res) {
+    let input = req.query.input;
+    let initNum = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
 
+    if (!initNum && !initUnit) {
+      res.send("invalid number and unit");
+      return;
+    } else if (!initNum) {
+      res.send("invalid number");
+      return;
+    } else if (!initUnit) {
+      res.send("invalid unit");
+      return;
+    }
+    
+    let returnNum = convertHandler.convert(initNum, initUnit);
+    let returnUnit = convertHandler.getReturnUnit(initUnit);
+    let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+
+    let responseObject = {}
+    responseObject['initNum'] = initNum
+    responseObject['initUnit'] = initUnit
+    responseObject['returnNum'] = returnNum
+    responseObject['returnUnit'] = returnUnit
+    responseObject['string'] = toString
+
+    res.json(responseObject);
+    // res.json({initNum, initUnit, returnNum, returnUnit, string: toString});
+  });
 };
